702cf3019cc962cbaf9ca3afe76cdc9ce431d2df
Ibraheem <highfrezh14@gmail.com>
2023-12-07 13:12:36 +0100

    training added

--- src/cnnClassifier/entity/__init__.py
+++ src/cnnClassifier/entity/__init__.py
@@ -1,1 +1,4 @@
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig, 
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
